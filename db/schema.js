b515687b6a935c66b6710294d28c9f50baa751d9
Jared Nichols <jnichols300@gmail.com>
2015-12-04 14:15:25 -0500

    created schema

---
+++ db/schema.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var UserSchema = new Schema ({
+    local: {
+      email: String,
+      password: String,
+    }
+      votes: [VoteSchema],
+      preferences: [PreferenceSchema]
+    }
+)};
+
+var VoteSchema = new Schema ({
+    location_id: String,
+    vote_flag: Boolean,
+    createdAt: {type: Date, default: Date.now},
+});
+
+var PreferenceSchema = new Schema ({
+    default_location: String,
+    removed_location: [],
+});
+
+var voteCountSchema = new Schema ({
+    location_id: String,
+    count: Number,
+});
--- public/js/views/testview.js
+++
@@ -1,7 +0,0 @@
-// loginTemplate: function(){
-//   var artist = this.artist;
-//   var html = $("<div>");
-//   html.append("<h3>" + location.name + "</h3>");
-// // Examples of what we want displayed under toggle?
-//   return(html);
-// },
