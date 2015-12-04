9b6b1ece40593d057ffc0d1bd7c9a22f6060d972
Jared Nichols <jnichols300@gmail.com>
2015-12-04 00:31:10 -0500

    added some basic html and css

---
+++ public/js/views/testview.js
@@ -0,0 +1,7 @@
+// loginTemplate: function(){
+//   var artist = this.artist;
+//   var html = $("<div>");
+//   html.append("<h3>" + location.name + "</h3>");
+// // Examples of what we want displayed under toggle?
+//   return(html);
+// },
