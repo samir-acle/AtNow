d331cc450d606bd797e5e8da1885287e720c221a
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-12-05 11:25:43 -0500

    Add signup page route for testing

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var app = express();
 var path = require("path");
 var mongoose = require("mongoose");
 var passport = require("passport");
@@ -9,31 +8,37 @@ var morgan = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+var app = express();
 
 mongoose.connect("mongodb://localhost/test")
-
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser());
+//
+// app.use(morgan('dev'));
+// app.use(cookieParser());
+// app.use(bodyParser());
 
 app.set('view engine', 'hbs');
 app.set("views","./views");
 app.use(express.static(__dirname + '/public'));
 
-app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+// app.use(session({ secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS' }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
 
-require('./config/passport')
+
+// require('./config/passport');
+// require("./config/routes");
 // comes up with error object is not a function, have not set up config passport yet
 // (passport);
-
 
 app.listen(3000, function(){
   console.log("listening on port 3000");
 });
 
-app.get("/", function(res, req){
+app.get("/", function(req, res){
   res.render("index.html");
+});
+
+app.get("/signup", function(req,res){
+  res.render("index.hbs");
 });
