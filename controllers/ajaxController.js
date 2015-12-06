6f547b4db12b868aab8b8f124bc80f810c99b5ec
Samir Mehta <sammehta88@gmail.com>
2015-12-05 21:58:54 -0500

    add http request to four square for locations

--- controllers/ajaxController.js
+++ controllers/ajaxController.js
@@ -1,17 +1,34 @@
 var express = require("express");
 var router = express.Router();
+var request = require("request");
+var env = require("../env")
 
 function error(response, message){
   response.status(500);
   response.json({error: message});
 }
 
-router.use(function(req, res, next) {
-  next();
-});
+// router.use(function(req, res, next) {
+//   next();
+// });
 
-router.get("/locations", function(req, res){
-  res.send("this is with locs");
+router.get("/", function(req, res){
+  var base = "https://api.foursquare.com/v2/venues/search";
+  var options = [
+    // ["intent", "checkin"],
+    // ["radius", 800], //meters of specified location
+    ["ll", 38.9 + "," + -77.0],
+    ["openNow", 1]
+  ];
+  var url = base + "?client_id=" + env.clientID + "&client_secret=" + env.clientSecret + "&v=20151203";
+
+  options.forEach(function(option){
+    url = url + "&"+ option[0] + "=" + option[1];
+  });
+
+  request(url, function(err, response, body) {
+    res.json(body);
+  });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -9,11 +9,16 @@ var bodyParser = require("body-parser");
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.json());
 
+app.use("*.json",function (req, res, next) {
+  req.headers.accept = 'application/json';
+  next();
+});
+
 app.get("/", function(req, res){
   res.render("index.html");
 });
 
-app.use("/locations", require("./controllers/ajaxController"));
+app.use("/locations/?:format?", require("./controllers/ajaxController"));
 
 app.listen(3000, function(){
   console.log("listening on port 3000");
