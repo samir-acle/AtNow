123d0cdfe84e402790173428916300a4c09def94
Jared Nichols <jnichols300@gmail.com>
2015-12-08 14:06:29 -0500

    adding toggle for locations

--- public/js/views/LocationView.js
+++ public/js/views/LocationView.js
@@ -1,6 +1,7 @@
+
 var LocationView = function(location) {
   this.loc = location;
-  this.$el = $("<li class='location'></li>");
+  this.$el = $("<div class='location'></div>");
 //   $(this).append('<input type="button" value="Upvote">').on("click", "button", function(){
 //
 // });
@@ -9,19 +10,45 @@ var LocationView = function(location) {
 LocationView.prototype = {
   render: function(){
     var self = this;
+
+    var showLocations = self.$el.find(".showLocations");
+    var locationsDiv = self.$el.find("div.locations");
+
+
+    showLocations.on("click", function(){
+        self.toggleLocations(locationsDiv);
+      });
+
     self.$el.append("<h3>" + self.loc.name + "</h3>");
-    self.$el.append('<input type="button" value="Upvote">');
+    self.$el.append('<input type="button" value="upvote" class="upvote">');
+    self.$el.append('<input type="button" value="downvote" class="downvote">');
     self.click();
     $('.loc-container').append(self.$el);
   },
   click: function(){
     var self = this;
-    self.$el.find('h3').on('click', function(){
-      console.log(self.loc);
-    });
-    self.$el.find('input').on('click', function(){
-      console.log(self.loc.id);
-      self.loc.postVote(self.loc.id);
-    });
+       self.$el.find('h3').on('click', function(){
+         console.log(self.loc);
+       });
+       self.$el.find('.upvote').on('click', function(){
+         self.loc.postVote(self.loc.id, true);
+       });
+       self.$el.find('.downvote').on('click', function(){
+         self.loc.postVote(self.loc.id, false);
+       });
+     },
+  toggleLocations: function(locationsDiv){
+    console.log(test);
+    var self = this;
+    if(locationDiv.children().length === 0){
+      this.Location.fetch().then(function(locations){
+        self.$el.append("<div>" + this.loc.icon + "</div>");
+        self.$el.append("<div>" + this.loc.address + "</div>");
+        self.$el.append("<div>" + this.loc.types + "</div>");
+        self.$el.append("<div>" + this.loc.rating + "</div>");
+      });
+    }
+    locationsDiv.toggle();
+    this.toggleDiv(locationsDiv);
   }
 };
