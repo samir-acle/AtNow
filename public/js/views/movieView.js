04828c30c7866adb6c23fde9be8e0b6a33b67eee
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-12-10 21:30:28 -0500

    Add CSS to Movie

--- public/js/views/movieView.js
+++ public/js/views/movieView.js
@@ -8,9 +8,10 @@ MovieView.prototype = {
     var self = this;
     var timeLeft = moment(self.movie.time).diff(moment(),'minutes');
     var timeClass = timeLeft > 15 ? 'green' : 'red';
-    self.$el.append('<span class="' + timeClass + '">in ' + timeLeft + 'min</span>');
-    self.$el.append('<span>' + self.movie.title + ' ' + moment(self.movie.time).format('h:mm A') + '</span>');
-    self.$el.append('<a href="' + self.movie.url + '">' + self.movie.theater + "</a>");
+    self.$el.append("<span class='movietitle'>" + self.movie.title + ' </span>');
+    self.$el.append('<span>' + moment(self.movie.time).format('h:mm A') + '</span>');
+    self.$el.append('<a href="' + self.movie.url + '"> @ ' + self.movie.theater + " </a>");
+    self.$el.append('<span class="' + timeClass + '">in ' + timeLeft + 'min </span>');
     $('.loc-container').append(self.$el);
   },
 };
